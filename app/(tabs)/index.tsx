2c90e3d23c90a0fef041738700c5525b381f3f77
uzair ghori <ukghori08@gmail.com>
2025-03-25 14:56:40 -0400

    chore: 🔥 remove HomePage component as part of cleanup

--- app/(tabs)/index.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function HomePage() { // Renamed component to HomePage
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#000000' }}>
-            <Text style={{ color: 'white', fontSize: 20 }}>Home Page Content</Text>
-        </View>
-    );
-}
